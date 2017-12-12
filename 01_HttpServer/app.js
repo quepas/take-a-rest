c065d822ea2a46e46073cab62a4ac389d7116596
quepas <kiepas@agh.edu.pl>
2017-12-12 23:56:08 +0100

    Adding commentaries to HttpServer code example

--- 01_HttpServer/app.js
+++ 01_HttpServer/app.js
@@ -1,18 +1,22 @@
 var express = require("express");
 var app = express();
 
+/* Handle main page */
 app.get("/", function(req, res) {
-  printRequest(req);
+  printReqSummary(req);
   res.send("<h2>Simple HTTP server</h2><p>Go to /hello subpage!</p>");
 });
 
+/* Handle `hello` subpage */
 app.get("/hello", function(req, res) {
-  printRequest(req);
+  printReqSummary(req);
   res.send("<h2>Special message</h2><p>Message for you: hello!</p>");
 });
 
-function printRequest(req) {
+/* Start HTTP server at port 3000 (type in the browser: http://localhost:3000/) */
+app.listen(3000);
+
+/* Helper function - print request summary */
+function printReqSummary(req) {
   console.log(`Handling ${req.method} ${req.originalUrl}`);
 }
-
-var server = app.listen(3000);
