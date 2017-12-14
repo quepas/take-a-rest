6933c09ebaf8591148541e0b4867459211c8b290
quepas <kiepas@agh.edu.pl>
2017-12-14 16:13:33 +0100

    Comments improvement

--- 01_HttpServer/app.js
+++ 01_HttpServer/app.js
@@ -3,23 +3,24 @@ var express = require("express");
 /* Create main app */
 var app = express();
 
-/* Handle main page */
+/* GET / -- Show main page */
 app.get("/", function(request, response) {
   printReqSummary(request);
   /* Send response to a request (here as a HTML markup) */
   response.send("<h2>HTTP Server</h2><p>Go to /hello subpage!</p>");
 });
 
-/* Handle `hello` subpage */
+/* GET /hello -- Show message */
 app.get("/hello", function(request, response) {
   printReqSummary(request);
   response.send("<p>Anonymous message: Oh, Hi Mark!</p>");
 });
 
-/* Start HTTP server at port 3000 (type in the browser: http://localhost:3000/) */
+/* Start HTTP server at port 3000
+    (type in the browser or use cURL: http://localhost:3000/) */
 app.listen(3000);
 
-/* Helper function - print request summary */
+/* Helper function -- print request summary */
 function printReqSummary(request) {
   /* Display handled HTTP method and link (path + queries) */
   console.log(`Handling ${request.method} ${request.originalUrl}`);
