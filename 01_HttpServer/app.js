c835707d5f86c3fefa66a4821e6bf5ac9342366e
quepas <kiepas@agh.edu.pl>
2017-12-15 10:44:02 +0100

    Taking into consideration neu5 remarks pt.1

--- 01_HttpServer/app.js
+++ 01_HttpServer/app.js
@@ -1,27 +1,27 @@
-/* Import Express web framework */
+// Import Express web framework
 var express = require("express");
-/* Create main app */
+// Create main app
 var app = express();
 
-/* GET / -- Show main page */
+// Helper function -- print request summary
+function printReqSummary(request) {
+  // Display handled HTTP method and link (path + queries)
+  console.log(`Handling ${request.method} ${request.originalUrl}`);
+}
+
+// GET / -- Show main page
 app.get("/", function(request, response) {
   printReqSummary(request);
-  /* Send response to a request (here as a HTML markup) */
-  response.send("<h2>HTTP Server</h2><p>Go to /hello subpage!</p>");
+  // Send response to a request (here as a HTML markup)
+  response.send("<h1>HTTP Server</h1><p>Go to /hello subpage!</p>");
 });
 
-/* GET /hello -- Show message */
+// GET /hello -- Show message
 app.get("/hello", function(request, response) {
   printReqSummary(request);
   response.send("<p>Anonymous message: Oh, Hi Mark!</p>");
 });
 
-/* Start HTTP server at port 3000
-    (type in the browser or use cURL: http://localhost:3000/) */
+// Start HTTP server at port 3000
+//  (type in the browser or use cURL: http://localhost:3000/)
 app.listen(3000);
-
-/* Helper function -- print request summary */
-function printReqSummary(request) {
-  /* Display handled HTTP method and link (path + queries) */
-  console.log(`Handling ${request.method} ${request.originalUrl}`);
-}
